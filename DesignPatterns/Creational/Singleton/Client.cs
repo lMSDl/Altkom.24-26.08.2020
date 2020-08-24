b4ce3ee09080d45d5d9fe9e5cd06eaf3eefc5d19
Paweł Łęczycki <pawel@msd.info.pl>
2020-08-24 16:32:24 +0200

    Singleton

---
+++ DesignPatterns/Creational/Singleton/Client.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Singleton
+{
+    public class Client
+    {
+        public static void Execute()
+        {
+            var db1 = Context.Instance.GetSettings("1");
+            Console.WriteLine($"Service: {db1}");
+            var db2 = Context.Instance.GetSettings("2");
+            Console.WriteLine($"Service: {db2}");
+        }
+    }
+}
---
+++ DesignPatterns/Creational/Singleton/Context.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.Creational.Singleton
+{
+    public class Context
+    {
+        private Dictionary<string, string> _settings = new Dictionary<string, string>();
+
+        private Context()
+        {
+            Console.WriteLine("Inicjalizacja singletona");
+
+            _settings.Add("1", "A");
+            _settings.Add("2", "B");
+            _settings.Add("3", "C");
+        }
+
+        public static Context Instance { get; } = new Context();
+
+        //private static Context instance;
+        //private static object locker = new object();
+
+        //public static Context GetInstace()
+        //{
+        //    lock (locker)
+        //    {
+        //        if (instance == null)
+        //            instance = new Context();
+        //    }
+        //    return instance;
+        //}
+
+
+        public string GetSettings(string key)
+        {
+            return _settings[key];
+        }
+    }
+}
