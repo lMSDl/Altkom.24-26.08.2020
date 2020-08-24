498608084d5d0179457fb96828307af0c58abf0f
Paweł Łęczycki <pawel@msd.info.pl>
2020-08-24 12:25:34 +0200

    ISP

---
+++ SOLID/IV_ISP.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLID
+{
+    interface IPdfFormatter
+    {
+        void ToPdf();
+    }
+
+    interface IExcelFormatter
+    {
+        void ToExcel();
+    }
+
+    class Report : IPdfFormatter, IExcelFormatter
+    {
+        public void ToExcel()
+        {
+            Console.WriteLine("Plik Excel wygenerowany!");
+        }
+
+        public void ToPdf()
+        {
+            Console.WriteLine("Plik Pdf Wygenerowany");
+        }
+    }
+
+    class Poem : IPdfFormatter
+    {
+        public void ToPdf()
+        {
+            Console.WriteLine("Plik Pdf Wygenerowany");
+        }
+    }
+}
